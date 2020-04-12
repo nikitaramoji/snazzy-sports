b86880d158c18021eb7a8f9d1d9cde3a5b275397
Daniel Kotroco <danielkotroco@Daniels-MBP.fios-router.home>
2020-04-12 18:54:16 -0400

    new files

--- scraping.py
+++ scraping.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup
 
 import requests
 import json
+import csv
 
-API_KEY = "8adcd451a0084c589292e7980a041d5c"
+API_KEY = "21c4b2ad2e76b2e7d6524a9bccbf5119"
 
 crp = CRP(API_KEY)
 data = {}
-company_names = ["Lockheed Martin", "Adobe Inc"]
+with open('/Users/danielkotroco/github/snazzy-sports/Value-Errors-Sheet1.csv', newline='') as f:
+    reader = csv.reader(f)
+    companies = list(reader)
 missing_companies = []
-cycle_years = ['2012', '2014', '2016', '2018']
+cycle_years = ['2012','2014', '2016', '2018']
+type_error_companies = []
 
 years_to_name_to_id = {}
 for year in cycle_years:
@@ -19,29 +23,42 @@ for year in cycle_years:
     years_to_name_to_id[year] = names_to_id
     f.close()
 
-for company_name in company_names:
+for company in companies:
     try:
-        org = crp.fetch('getOrgs', org=company_name)
-        org_id = org['organization']['@attributes']['orgid']
+        company_name = company[0]
+        org_id = company[1]
         data[company_name] = {}
         for year in cycle_years:
-            data[company_name][year] = []
-            cycle_url = "https://www.opensecrets.org/orgs/recips.php?cycle=" + year + "&id=" + org_id
-            response = requests.get(cycle_url)
-            response.raise_for_status()
-            soup = BeautifulSoup(response.content, 'html.parser')
-            full_candidate_info_table = soup.find(id="recips").find("tbody").find_all("tr")
-            for candidate_info in full_candidate_info_table:
-                name = candidate_info.find_all("td")[0].text.split(" ")
-                # print(year, name)
-                # cid = years_to_name_to_id[year][name[0] + ' ' + name[1]]
-                data[company_name][year].append(str(name[1] + " " + name[0][:-1]))
-
+            try:
+                data[company_name][year] = []
+                cycle_url = "https://www.opensecrets.org/orgs/recips.php?cycle=" + year + "&id=" + org_id
+                response = requests.get(cycle_url)
+                response.raise_for_status()
+                soup = BeautifulSoup(response.content, 'html.parser')
+                full_candidate_info_table = soup.find(id="recips").find("tbody").find_all("tr")
+                for candidate_info in full_candidate_info_table:
+                        name = candidate_info.find_all("td")[0].text.split(" ")
+                        # print(year, name)
+                        # cid = years_to_name_to_id[year][name[0] + ' ' + name[1]]
+                        data[company_name][year].append(str(name[1] + " " + name[0][:-1]))
+            except AttributeError as e:
+                print(e)
+                print(company_name + " could not be found (AttributeError)" + year)
     except ValueError as e:
         print(e)
-        print(company_name + " could not be found")
+        print(company_name + " could not be found (ValueError)")
         missing_companies.append(company_name)
+    except TypeError as e:
+        print(e)
+        print(company_name + " could not be found (TypeError)")
+        type_error_companies.append(company_name)
 
 json_data = json.dumps(data)
 
-print(data["Lockheed Martin"]['2012'])
+print(missing_companies)
+print(len(missing_companies))
+print(type_error_companies)
+print(len(type_error_companies))
+f = open("company_opensecrets_dataFINAL","w")
+f.write(json_data)
+f.close()
